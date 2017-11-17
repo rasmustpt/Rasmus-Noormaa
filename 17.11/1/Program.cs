81e7b602e1d832c295ecb095388a9fa25367276b
rasmustpt <rasmus.noormaa@@tptlive.ee>
2017-11-17 14:15:33 +0200

    when
    123

---
+++ 17.11/1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo");
+            Console.WriteLine("Demo2");
+            Console.WriteLine("Demo3");
+
+            Console.ReadLine();
+        }
+    }
+}
