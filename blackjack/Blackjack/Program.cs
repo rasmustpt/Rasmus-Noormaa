43bd3954c9b9c1e652646c0e2e99bf88aeeca836
rasmustpt <rasmus.noormaa@tptlive.ee>
2017-12-03 21:36:05 +0200

    Blackjack
    My progress so far

--- blackjack/Blackjack/Program.cs
+++ blackjack/Blackjack/Program.cs
@@ -1,9 +1,7 @@
 ﻿/*
- * 
  * kuidas pildid lisada kaardipakki
- * kuidas talletada summasse kaart
- * üks maja kaartidest ära 'peita' (ka meetod 'proceed')
- * 'finish' meetod
+ * üks maja kaartidest ära 'peita'
+ * keel ühtlustada
  */
 
 using System;
@@ -12,7 +10,6 @@ namespace Blackjack
 {
     class Program
     {
-
         public static int Number()
         {
             int[] numbrid = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
@@ -35,41 +32,82 @@ namespace Blackjack
             return mast;
         }
 
-        public static void Proceed()
+        public static void Finish(int your_summa, int my_summa)
         {
-            Console.WriteLine("What do you want to do now?\n");
-            Console.WriteLine("a. draw one more card?");
-            Console.WriteLine("b. finish?");
+            Console.WriteLine($"\nYou have {your_summa} points");
+            Console.WriteLine($"I have {my_summa} points\n");
 
-            string input = Console.ReadLine();
+            if (your_summa > my_summa && (your_summa < 21 && my_summa > 21))
+                Console.WriteLine("You win!");
 
-            if (input == "a")
-            {
-                Console.WriteLine($"\nYour next card is {Number() + Mast()}");
-                Console.WriteLine($"My next card is {Number() + Mast()}\n\n");
-                Proceed();
-            }
-            if (input == "b")
-            {
-                Finish();
-            }
-        }
+            if (your_summa < my_summa && (my_summa < 21 && your_summa > 21))
+                Console.WriteLine("I win!");
 
-        public static void Finish()
-        {
-            /*KUIDAS*/
+            if (your_summa == my_summa && your_summa < 21 && my_summa < 21)
+                Console.WriteLine("Tie!");
+
+            if (your_summa > 21 && my_summa > 21)
+                Console.WriteLine("We are both burst, so house wins!");
+
+            Console.ReadLine();
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, this is blackjack game. Good luck!\n\n");
 
-            /*kuidas talletada summa?*/
+            int a = Number();
+            int b = Number();
 
-            Console.WriteLine($"Your cards are {Number()+Mast()} and {Number()+Mast()}");
-            Console.WriteLine($"My cards are {Number()+Mast()} and {Number()+Mast()}\n\n");
+            int c = Number();
+            int d = Number();
 
-            Proceed();
+            Console.WriteLine($"Your cards are {a+Mast()} and {b+Mast()}");
+            Console.WriteLine($"My cards are {c+Mast()} and {d+Mast()}\n\n");
+
+            int your_summa = a + b;
+            int my_summa = c + d;
+
+            Console.WriteLine("What do you want to do now?\n");
+            Console.WriteLine("a. draw one more card?");
+            Console.WriteLine("b. finish?\n");
+
+            string input = Console.ReadLine();
+
+            bool väärtus = true;
+            while (väärtus)
+            {
+                if (input == "a")
+                {
+                    int e = Number();
+                    int f = Number();
+
+                    Console.WriteLine($"\nYour next card is {e + Mast()}");
+                    Console.WriteLine($"My next card is {f + Mast()}\n\n");
+
+                    your_summa = your_summa + e;
+                    my_summa = my_summa + f;
+
+                    Console.WriteLine("What do you want to do now?\n");
+                    Console.WriteLine("a. draw one more card?");
+                    Console.WriteLine("b. finish?\n");
+
+                    string input2 = Console.ReadLine();
+
+                    if (input2 == "b")
+                    {
+                        break;
+
+                        /*Finish(your_summa, my_summa);*/
+                    }
+                }
+                if (input == "b")
+                {
+                    break;
+                    
+                } 
+            }
+            Finish(your_summa, my_summa);
 
             Console.ReadLine();
         }
